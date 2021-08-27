2a446e0d66292dfba0d11eb5306bf3d720cdccc7
zmrbak <20995658@qq.com>
2021-08-27 12:08:08 +0800

    add code

---
+++ 配套代码/L02/RE302/RE302.cpp
@@ -0,0 +1,11 @@
+﻿// RE302.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+	//std::cout << "Hello World!\n";
+	std::cout << "Hello" << " World!\n";
+	//printf("Hello World!\n");
+}
